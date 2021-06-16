7925c37803c8bccacb13cc26a2c93a1a1d4dddfa
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-16 20:12:41 +0200

    Update Pawn object

--- Pawn.cpp
+++ Pawn.cpp
@@ -5,7 +5,7 @@ bool Pawn::checkMove(int dest_x, int dest_y/*, std::vector<std::vector<Piece*>>b
 	/*tengelyeket megcserelni*/
 	if (std::abs(dest_y - this->getY()) != 0 || std::abs(dest_x - this->getX()) != 1) {
 		res = false;
-		std::cout << "rossz lepes\n";
+		throw(std::runtime_error("Invalid Step!"));
 	}
 	/*strifos megoldast implementalni*/
 	return res;
