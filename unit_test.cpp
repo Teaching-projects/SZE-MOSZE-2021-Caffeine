f7192b969fe3636ae392f446f5578482e9bbc9cd
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-16 19:42:23 +0200

    Add gtest

---
+++ unit_test.cpp
@@ -0,0 +1,25 @@
+#include "../JSON.h"
+#include "../Piece.h"
+#include "../Piece.cpp"
+#include "../King.h"
+#include "../King.cpp"
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <utility>
+
+TEST(King_piece,King_exists)
+{
+	Piece *tmp_piece = new King(1, 1);
+	int x = tmp_piece->getX();
+	int y = tmp_piece->getY();
+	EXPECT_EQ(x,1);
+	EXPECT_EQ(y,1);
+}
+
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
