6939df6bed6bcff610b1955493f66c9b805aff6f
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-17 21:10:32 +0200

    Expand Game object

--- Game.h
+++ Game.h
@@ -7,21 +7,18 @@ class Game {/**
 			 */
 	Board *palya;
 	bool gameover = false;
+	const std::string inputSource;
 public:
-	Game() { start();}
+	Game(const std::string& inputFile) : inputSource(inputFile) { start(); }
 	~Game() { delete palya; }
 	void start();
 
 	bool isgameover() { return this->gameover; }
 	void setgame() { gameover = true; }
+	
+	std::string getInput()
+	{
+		return this->inputSource;
+	}
 };
 
-/*
-A játékmenet a következõ :
-	1. Board.h setup függvény meghívása
-	2. egy While(!gameover) ciklus inputokat bekérni felhasználóktól felváltva
-	3. inputonként gameovert ellenõrzni.
-
-
-Mikor van vége a játéknak ? - ha az egyik királyt leütjük.
-*/
