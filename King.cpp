50608cacf1dd0c4d4155ea27d32c0232bc2e83d4
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-17 21:05:38 +0200

    Fix unique character problem

--- King.cpp
+++ King.cpp
@@ -1,13 +1,35 @@
-#include"King.h"
 
-bool King::checkMove(int dest_x, int dest_y) {
-	/*method to filter bad moves*/
-	bool res = true;
-	int distance = std::abs(dest_x - getX()) + std::abs(dest_y - getY());
-	if (distance != 1) {
-		res = false;
+#include "Knight.h"
+
+bool Knight::checkMove(int dest_x, int dest_y) {
+	
+	bool res = false;
+	int dist = abs((this->getX() - dest_x) + (this->getY()- dest_y));
+	if (dist == 3 && this->getX() - dest_x != 0 && this->getY() - dest_y != 0) {
+		res = true;
+	}
+	else {
 		throw(std::runtime_error("Invalid Step!"));
 	}
-
 	return res;
+}
+ std::string  Knight::toString() {
+
+	 std::string str = "";
+	 if (this->get_isWhite())
+	 {
+		 str.append("W");
+		 str.append("N");
+		 str.append("  ");
+	 }
+	 else {
+		 str.append("B");
+		 str.append("N");
+		 str.append("  ");
+	 }
+	 return str;
+
+
+
+
 }
