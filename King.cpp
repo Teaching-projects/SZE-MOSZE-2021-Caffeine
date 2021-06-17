3cde5d06576a7321c95210d43791f036717467b0
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-17 19:40:15 +0200

    Rewrite comment

--- King.cpp
+++ King.cpp
@@ -1,7 +1,7 @@
 #include"King.h"
 
 bool King::checkMove(int dest_x, int dest_y) {
-	/*a kiraly lephet barmilyen tablaban levo mezoben, ammenyiben az 1 egyseg tavolsagra van.*/
+	/*method to filter bad moves*/
 	bool res = true;
 	int distance = std::abs(dest_x - getX()) + std::abs(dest_y - getY());
 	if (distance != 1) {
