57e96e1e479effc41cc52efdb7de76ce76ee7b29
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-17 19:42:20 +0200

    Add comment

--- Pawn.cpp
+++ Pawn.cpp
@@ -1,8 +1,8 @@
 #include "Pawn.h"
 
 bool Pawn::checkMove(int dest_x, int dest_y/*, std::vector<std::vector<Piece*>>board)*/) {
+	/*method to filter bad moves*/
 	bool res = true;
-	/*tengelyeket megcserelni*/
 	if (std::abs(dest_y - this->getY()) != 0 || std::abs(dest_x - this->getX()) != 1) {
 		res = false;
 		throw(std::runtime_error("Invalid Step!"));
