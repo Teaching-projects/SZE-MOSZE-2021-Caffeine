5b8f6e44069565705f883c84684533b52fe4bfe2
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-17 19:44:04 +0200

    Rewrite comment

--- Rook.cpp
+++ Rook.cpp
@@ -1,7 +1,7 @@
 #include "Rook.h"
 
 bool Rook::checkMove(int dest_x, int dest_y) {
-	/*a bástya ugyanabban a sorban vagy oszlopban léphet bármennyit amelyikben éppen aktuálisan tartózkodik*/
+	/*method to filter bad moves*/
 	bool res = false;
 	if ((this->getX() - dest_x != 0) && (this->getY() - dest_y == 0) || (this->getY() - dest_y != 0) && (this->getX() - dest_x == 0)) {
 		res = true;
