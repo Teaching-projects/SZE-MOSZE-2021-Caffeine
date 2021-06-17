db9268929ef418fec5153415d401682e01d86ec9
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-17 20:12:25 +0200

    Add comment

--- Nullpiece.h
+++ Nullpiece.h
@@ -6,6 +6,7 @@ class Nullpiece : public Piece {
 
 //	int curr_x, curr_y;
 public:
+	/*class contains an empty square on the table*/
 	Nullpiece(int x_pos, int y_pos) : Piece(x_pos,y_pos,"Null") {}
 	~Nullpiece() {}
 	void Abstract() { return; }
