bce68379727592316b4bba23c114296fcf95478c
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-17 21:04:53 +0200

    Fix endgame bug

--- Game.cpp
+++ Game.cpp
@@ -7,14 +7,13 @@ void Game::start() {
 	palya->setup();
 	palya->toString();
 
-	/*!gameplayt függvény kent folytatni*/
+
 	try {
 		auto steps_from_file = JSON::parseJson(getInput());
 		for (int i = 0; i < steps_from_file.size(); ++i) {
 
 			for (auto &it : steps_from_file[i])
 			{
-				std::cout << it.first << "\t" << it.second << std::endl;
 				koords.push_back(it.second);
 
 			}
@@ -26,23 +25,23 @@ void Game::start() {
 		std::cout << e.what() << std::endl;
 
 	}
+
+	for (int i = 0; i < koords.size(); i = i + 4) {
+		palya->move_piece(koords[i], koords[i + 1], koords[i + 2], koords[i + 3]);
+	}
 	if (palya->checkGameOver())
 		setgame();
 	while (!isgameover()) {
 
 
-		for (int i = 0; i < koords.size(); i = i + 4) {
-			palya->move_piece(koords[i], koords[i+1], koords[i+2], koords[i+3]);
-		}
+
 		std::cin >> piece_x >> piece_y >> dest_x >> dest_y;
-		//ide a babuknak a parancsot átadó függvény
+
 
 		
 
 		palya->move_piece(piece_x, piece_y, dest_x, dest_y);
-		/*ide a while loopal végig járni a json olvasás eredményét*/
-		if (dest_x == 0 && dest_y == 0)
-			setgame();
+
 	}
 
 }
