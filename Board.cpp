d41c1e50d15e60ca8f65d693d0b0d3020cf18be9
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-18 21:29:31 +0200

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -153,11 +153,13 @@ void Board::move_piece(int px, int py, int dx, int dy) {
 
 	try {
 		if (board[px][py]->checkMove(dx, dy)) {
-			board[px][py]->move(*board[px][py], dx, dy);
+			
 
 			
 
 			auto dummy = board[px][py];
+			
+			board[px][py]->move(*board[px][py], dx, dy);
 			board[dx][dy] = dummy;
 			delete board[dx][dy];
 			board[px][py] = new Nullpiece(px, py);
