accbcdd16b49416ff2976a74c4494f05d7571d3f
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:16:58 +0200

    Add comments

--- Bishop.h
+++ Bishop.h
@@ -3,9 +3,15 @@
 #include"Piece.h"
 
 class Bishop : public Piece {
+	/*
+	*the class contains the bishop type pieces' specific methods
+	*/
 public:
 	Bishop(int x_pos, int y_pos) : Piece(x_pos, y_pos, "Bishop") {}
 	~Bishop() {}
 	void Abstract() { return; }
 	bool checkMove(int dest_x, int dest_y);
+	/*
+	*a bishop can move several steps in a diagonal direction
+	*/
 };
