f5e59e9fa40f7e99e7e19bcdf883e0a49c55c60f
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:52:06 +0200

    Add comment

--- King.h
+++ King.h
@@ -2,15 +2,12 @@
 #include"Piece.h"
 class King :public Piece {
 	/**
-	*
+	*this class stores the methods specific for the king piece type
 	*/
 
 public:
 	King(int x_pos, int y_pos) : Piece(x_pos,y_pos,"King") {}
 	~King() {}
 	void Abstract() {return;}
-
 	bool checkMove(int dest_x, int dest_y);
-
-
 };
