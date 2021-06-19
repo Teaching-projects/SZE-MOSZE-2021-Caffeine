adeaf38e81877b68a28be6d371b6401b554aa344
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:50:38 +0200

    Add comments

--- King.cpp
+++ King.cpp
@@ -1,9 +1,14 @@
 #include"King.h"
 
 bool King::checkMove(int dest_x, int dest_y) {
-	/*a kiraly lephet barmilyen tablaban levo mezoben, ammenyiben az 1 egyseg tavolsagra van.*/
+	/**
+	*king can move 1 square in any direction
+	*/
 	bool res = true;
 	int distance = std::abs(dest_x - getX()) + std::abs(dest_y - getY());
+	/**
+	*if the distance of the new position is not exactly 1 from the original, its not a valid move
+	*/
 	if (distance != 1) {
 		res = false;
 		throw(std::runtime_error("Invalid Step!"));
