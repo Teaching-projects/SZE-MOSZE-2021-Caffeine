df9009d4ef01807584a5f0dd7ca1d1a053677eb3
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:57:19 +0200

    Add comments

--- Knight.cpp
+++ Knight.cpp
@@ -2,11 +2,16 @@
 #include "Knight.h"
 
 bool Knight::checkMove(int dest_x, int dest_y) {
-	
+	/**
+	*the knight can move 2 squares on an axis, and 1 on the other, both needs to happen, but the order can be interchangeable
+	*/
 	bool res = false;
 	int dist = abs((this->getX() - dest_x) + (this->getY()- dest_y));
 	if (dist == 3 && this->getX() - dest_x != 0 && this->getY() - dest_y != 0) {
 		res = true;
+		/**
+		*if the distance is exactly 3 and neither the x, nor the y is zero, only then it is a valid move
+		*/
 	}
 	else {
 		throw(std::runtime_error("Invalid Step!"));
