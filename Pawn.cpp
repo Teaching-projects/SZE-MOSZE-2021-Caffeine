172d40b01b1ff35170feea8dcd3d4e2673239d76
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 19:06:21 +0200

    Add comments

--- Pawn.cpp
+++ Pawn.cpp
@@ -1,11 +1,16 @@
 #include "Pawn.h"
 
 bool Pawn::checkMove(int dest_x, int dest_y) {
+	/**
+	*pawns can only move 1 square on the x axis
+	*/
 	bool res = true;
-	
 	if (std::abs(dest_y - this->getY()) != 0 || std::abs(dest_x - this->getX()) != 1) {
 		res = false;
 		throw(std::runtime_error("Invalid Step!"));
+		/**
+		*if the pawn doesn't only move 1 square on the x, then it's not a valid move
+		*/
 	}
 	
 	return res;
