dcc9d893ed3fd9f697a528fb9d920cddc042e109
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:59:29 +0200

    Add comment

--- Main.cpp
+++ Main.cpp
@@ -7,16 +7,14 @@
 
 int main(int argc, char* argv[])
 {
-
-
-
+	/**
+	*the main function for the game
+	*/
 	std::string tmp = argv[1];
 	if(tmp.empty())
 		throw(std::runtime_error("Missing argument!"));
 	
 	Game *ujgame = new Game(argv[1]);
-	/*ujgame->start();*/
-
 	delete ujgame;
 	return 0;
 }
