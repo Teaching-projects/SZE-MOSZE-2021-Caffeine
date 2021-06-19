65e82cde6638a2b0d90c631363c69360e5d00e33
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:19:00 +0200

    Add comments

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,12 +1,19 @@
 #include "Bishop.h"
 
 bool Bishop::checkMove(int dest_x, int dest_y) {
-	/*method for catching bad moves*/
+	/*
+	*method for catching bad moves
+	*/
 	bool res = true;
 	if (abs(this->getX() - dest_x) != abs(this->getY() - dest_y)) {
+		/*
+		*the absolute value of the distance covered with the move needs to be equal on both x and y
+		*/
 		res = false;
 		throw(std::runtime_error("Invalid Step!"));
-		/*if the input doesn't match the bishop movement pattern, the program doesn't allow the input to go through*/
+		/*
+		*if the input doesn't match the bishop movement pattern, the program doesn't allow the input to go through
+		*/
 	}
 	return res;
 }
