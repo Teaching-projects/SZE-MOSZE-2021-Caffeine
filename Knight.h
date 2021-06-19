b68cd2de16e2e1e1c9956df5090eee3d12ebdaf7
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:58:08 +0200

    Add comment

--- Knight.h
+++ Knight.h
@@ -2,6 +2,9 @@
 #include"Piece.h"
 
 class Knight : public Piece {
+	/**
+	*this class stores the methods specific for the knight pieces
+	*/
 public:
 	Knight(int x_pos, int y_pos) : Piece(x_pos, y_pos, "Knight") {}
 	~Knight() {}
