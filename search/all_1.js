f916c666fd20ad1ed7734cae214acc445bdc4afb
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:51:10 +0200

    Update gh-pages to output generated at adeaf38

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['checkgameover_2',['checkGameOver',['../class_board.html#a52fb04fce1c17a4e49d4fdd890ab2b9a',1,'Board']]],
-  ['checkmove_3',['checkMove',['../class_bishop.html#ab007484a4642b610f099a29dd7d443e2',1,'Bishop']]]
+  ['checkmove_3',['checkMove',['../class_bishop.html#ab007484a4642b610f099a29dd7d443e2',1,'Bishop::checkMove()'],['../class_king.html#a5ec60cab71c11bc607e177bf65da3196',1,'King::checkMove()']]]
 ];
