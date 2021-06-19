02ef90792f70264cd200189c262be2180038f3dc
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 18:12:46 +0200

    Add comments

--- Bishop.cpp
+++ Bishop.cpp
@@ -1,11 +1,12 @@
 #include "Bishop.h"
 
 bool Bishop::checkMove(int dest_x, int dest_y) {
-	/*a futó léphet bármerre átlósan*/
+	/*method for catching bad moves*/
 	bool res = true;
 	if (abs(this->getX() - dest_x) != abs(this->getY() - dest_y)) {
 		res = false;
 		throw(std::runtime_error("Invalid Step!"));
+		/*if the input doesn't match the bishop movement pattern, the program doesn't allow the input to go through*/
 	}
 	return res;
 }
