b33f5274feaa4975a238f1bcd10734594fc287aa
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2021-06-19 19:10:48 +0200

    Add comments

--- Piece.cpp
+++ Piece.cpp
@@ -1,6 +1,9 @@
 #include "Piece.h"
 
 std::string Piece::toString(){
+	/**
+	*method for differentiating between black and white pieces for the user
+	*/
 	if (!get_leftbehind()) {
 		char k = name[0];
 		std::string kezdo{ k };
@@ -24,7 +27,9 @@ std::string Piece::toString(){
 	}
 }
 void Piece::move(Piece& p, int dest_x, int dest_y) {
-
+	/**
+	*method for moving pieces on the board
+	*/
 	p.curr_x = dest_x;
 	p.curr_y = dest_y;
 
